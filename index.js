ac0c264f3b4d056bfcf231f3343bd4ef06a76d31
Sudhanshu <ersshekhar05@gmail.com>
2023-09-08 21:49:45 +0530

    updated code push

---
+++ index.js
@@ -0,0 +1,20 @@
+// const slider = document.querySelector(".slider");
+// const slides = document.querySelectorAll(".slide");
+// const prevBtn = document.querySelector(".prev-slide");
+// const nextSlide = document.querySelector(".next-slide");
+
+// let slideIndex = 0;
+
+// slides[slideIndex].classList.add("active");
+
+// // prevBtn.addEventListener("click", prevSlideFunc);
+// nextSlide.addEventListener("click", nextSlideFunc);
+
+
+// function nextSlideFunc(){
+//     slides[slideIndex].classList.remove('active');
+//     slideIndex = (slideIndex === slides.length-1) ? 0 : slideIndex+1;
+//     slides[slideIndex].classList.add("active");
+//     slider.style.transform = `translateX(-${slideIndex*100}%)`;
+
+// }
