46c8c1a8a01af0052368f0ee8e8d38601d624127
Sudhanshu <ersshekhar05@gmail.com>
2023-09-26 15:25:29 +0530

    updated code

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-// const slider = document.querySelector(".slider");
-// const slides = document.querySelectorAll(".slide");
-// const prevBtn = document.querySelector(".prev-slide");
-// const nextSlide = document.querySelector(".next-slide");
+let slider_Box = document.getElementById("slider_Box");
 
-// let slideIndex = 0;
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
-// slides[slideIndex].classList.add("active");
+const arr = ["https://snacknation.com/wp-content/uploads/2020/12/Best-Task-Management-Software-Platforms.png", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbzEnu5dlONOmSgTtByQOZyE4JQAHNjwiNow&usqp=CAU.png"];
 
-// // prevBtn.addEventListener("click", prevSlideFunc);
-// nextSlide.addEventListener("click", nextSlideFunc);
+const link = ["https://sudhanshu735591.github.io/Task-Manager-App---Javascript-Project---ma50o6beiti5/", "https://sudhanshu735591.github.io/Tic-Tac-Toe-Game/"];
+
+let flag = 0;
+let img = document.createElement("img");
+img.src = arr[flag];
+img.alt = "ticTacToe"
+
+img.className = "carousel_image";
+slider_Box.appendChild(img);  
 
 
-// function nextSlideFunc(){
-//     slides[slideIndex].classList.remove('active');
-//     slideIndex = (slideIndex === slides.length-1) ? 0 : slideIndex+1;
-//     slides[slideIndex].classList.add("active");
-//     slider.style.transform = `translateX(-${slideIndex*100}%)`;
+next.addEventListener("click", ()=>{
+    slider_Box.innerHTML = "";
+    flag++;
+    if(flag===arr.length){
+        flag = 0;
+    }
 
-// }
+    let a = document.createElement("a");
+    a.href = link[flag];
+
+    let img = document.createElement("img");
+    img.src = arr[flag];
+    img.className = "carousel_image";
+    a.appendChild(img);
+    slider_Box.appendChild(a);    
+})
+
+
+prev.addEventListener("click", ()=>{
+    slider_Box.innerHTML = "";
+    if(flag===-1){
+        flag=arr.length-1;
+    }
+    console.log(flag);
+    let a = document.createElement("a");
+    a.href = link[flag];
+
+
+    let img = document.createElement("img");
+    img.src = arr[flag];
+    img.className = "carousel_image";
+    
+    a.appendChild(img);
+    slider_Box.appendChild(a);    
+    flag--;
+})
+
+
